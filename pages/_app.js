cfa47e7e5fd04a999e5fa3de77b612ccca6d8003
Chekkal Youcef <chekkalislam@gmail.com>
2023-04-17 00:17:12 +0100

    added Vercel Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,17 @@
-import { ThemeProvider } from 'next-themes'
-import Layout from '../components/Layout'
-import '../styles/globals.css'
+import { ThemeProvider } from "next-themes";
+import Layout from "../components/Layout";
+import { Analytics } from "@vercel/analytics/react";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider attribute="class">
-      
       <Layout>
         <Component {...pageProps} />
+        <Analytics />
       </Layout>
     </ThemeProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
